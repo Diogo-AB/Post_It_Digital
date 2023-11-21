503523aad70f46d2d5f7ecc4e676fa4965ad9a2a
Adalberto Santos <adalbertosantospinheiro@outlook.com>
2023-11-21 19:46:56 -0300

    comecando a fazer a home

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,12 @@
 
 from django.urls import path
 from . import views
+from .views import home_view
 
 
 urlpatterns = [
     
+    path('', home_view, name='home'),
     path('AcessarConta/', views.AcessarConta, name= 'Conta'),
     path('MinhasAnotacoes/', views.MinhasAnotacoes, name='Anotacoes'),
     
--- home/views.py
+++ home/views.py
@@ -1,14 +1,21 @@
 #views da home 
 
 from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
+
 # Create your views here.
 
+def home_view(request):
+    # Configurando o local para português do Brasil
+    
+    data_atual = datetime.now().date() 
+    return render(request, 'home.html', {'data_atual': data_atual})
+
 def AcessarConta(request):
-    return HttpResponse()
+    return render(request, 'home.html')
 
 
 def MinhasAnotacoes(request):
-    
-    
- return HttpResponse()
+    return render(request, 'home.html')
+   
