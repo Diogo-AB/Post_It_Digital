23e4f274130968f307a98d3eedf94072c4b5934c
Adalberto Santos <adalbertosantospinheiro@outlook.com>
2023-11-23 17:29:26 -0300

    registrar feito, mas com bugs, (codigo funcionando perfeito o resto)

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 #views da home 
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -10,15 +11,19 @@ def home_view(request):
     user_name = request.user.username if request.user.is_authenticated else None
     return render(request, 'home.html', {'user_name': user_name})
 
+
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')  # Substitua 'home' pelo nome da sua página inicial
+            return redirect('/')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
 
     return render(request, 'register.html', {'form': form})
    
+def Logout_View(request):
+    logout(request)
+    return redirect('/')
